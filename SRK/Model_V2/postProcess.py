41790ad94eb79006dd03c170b0f89abe56bb0cc1
SudalaiRajkumar <ssraj.rox@gmail.com>
2015-09-18 22:12:05 +0530

    Create postProcess.py

---
+++ SRK/Model_V2/postProcess.py
@@ -0,0 +1,42 @@
+import csv
+import numpy as np
+import pandas as pd
+
+if __name__ == "__main__":
+	file_name = "test_predictions_rf_est200_dep25_leaf7_feat3_seed0_trainseed1234.csv"
+	sample_sub_file = "../../Data/sample_submission.csv"
+	sub_file_name = "sub_rf_est200_dep25_leaf7_feat3_seed0_trainseed1234.csv"
+
+	file_handle = open(file_name,"rb")
+	sub_file_handle = open(sub_file_name, "wb")
+	user_ids_list = np.array( pd.read_csv(sample_sub_file)["USER_ID_hash"] )
+
+	reader = csv.DictReader(file_handle)
+	writer = csv.writer(sub_file_handle)
+	writer.writerow(["USER_ID_hash","PURCHASED_COUPONS"])
+
+	print "Getting user dict.."
+	user_dict = {}
+	for row in reader:
+		user_id = row["USER_ID_hash"]
+		temp_dict = user_dict.get(user_id, {})
+		temp_dict[float(row["Prediction"])] = row["COUPON_ID_hash"]
+		user_dict[user_id] = temp_dict
+
+	print "Writing to out file.."
+	for user_id in user_ids_list:
+		temp_dict = user_dict[user_id]
+		pred_values_list = list(temp_dict.keys())
+		pred_values_list.sort()
+		pred_values_list.reverse()
+		#print pred_values_list[:10]
+		purchased_coupons = []
+		for i in xrange(10):
+			#if pred_values_list[i]>0.2:
+				purchased_coupons.append( temp_dict[pred_values_list[i]] )
+		purchased_coupons = " ".join(purchased_coupons)
+		writer.writerow([user_id, purchased_coupons])
+			 
+
+	file_handle.close()
+	sub_file_handle.close()	
